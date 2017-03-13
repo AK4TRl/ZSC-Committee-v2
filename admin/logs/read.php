ecf903d5c344a7b98d040f4a53dc33a5de5b594e
LiuACG <LiuACG@gmail.com>
2017-03-13 22:08:35 +0800

    + add style, script & image files to admin asset folder

---
+++ admin/logs/read.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LiuACG
+ * Date: 2017/3/13
+ * Time: 20:56
+ */
---
+++ admin/utils/Log.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: LiuACG
+ * Date: 2017/3/13
+ * Time: 20:09
+ */
+class Log
+{
+
+}
